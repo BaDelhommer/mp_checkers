6c21967e80ac6c15a1c2186afaea2e120e635b50
BaDelhommer <badelhommer@gmail.com>
2024-07-21 15:42:29 -0500

    added select anc change turns methods

--- game.go
+++ game.go
@@ -8,3 +8,38 @@ type Game struct {
 	Turn       rl.Color
 	ValidMoves [][]*Piece
 }
+
+func (g *Game) Select(row, col int32) bool {
+	if g.Selected != nil {
+		result := g.move(row, col)
+		if !result {
+			g.Selected = nil
+			g.Select(row, col)
+		}
+	}
+
+	piece := g.Board.getPiece(row, col)
+	if !piece.Empty && piece.Color == g.Turn {
+		g.Selected = piece
+		g.ValidMoves = g.Board.GetValidMoves(piece)
+		return true
+	}
+	return false
+}
+
+func (g *Game) changeTurn() {
+	if g.Turn == rl.Red {
+		g.Turn = rl.White
+	} else {
+		g.Turn = rl.Red
+	}
+}
+
+func NewGame() *Game {
+	return &Game{
+		Board:      Board{},
+		Selected:   nil,
+		Turn:       rl.Red,
+		ValidMoves: [][]*Piece{},
+	}
+}
